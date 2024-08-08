bf2761a737d30db0676cc600ac0782b75ba15841
nsantacruz <noahssantacruz@gmail.com>
2024-08-08 09:41:58 +0300

    add function to guess optimal n clusters

--- util.py
+++ util.py
@@ -2,6 +2,9 @@ from typing import Any, Callable, Optional, Sequence
 import re
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
+from sklearn.metrics import silhouette_score
+from sklearn.base import ClusterMixin
+from numpy import ndarray
 
 
 def get_by_xml_tag(text, tag_name) -> Optional[str]:
@@ -26,3 +29,35 @@ def run_parallel(items: Sequence[Any], unit_func: Callable, max_workers: int, **
 
     output = [future.result() for future in futures if future.result() is not None]
     return output
+
+
+def guess_optimal_n_clusters(embeddings: ndarray, get_model: Callable[[int], ClusterMixin], verbose=False) -> int:
+    """
+    Utility function to guess optimal number of clusters for a given cluster model.
+    Useful for models that require number of clusters as input.
+    :param embeddings: ndarray of embeddings of shape (n_samples, n_features)
+    :param get_model: Callable that takes number of clusters as parameter and returns an sklearn cluster model
+    :param verbose:
+    :return: optimal number of clusters based on silhouette score
+    """
+    if len(embeddings) <= 1:
+        return len(embeddings)
+
+    best_sil_coeff = -1
+    best_num_clusters = 0
+    MAX_MIN_CLUSTERS = 3  # the max start of the search for optimal cluster number.
+    n_cluster_start = min(len(embeddings), MAX_MIN_CLUSTERS)
+    n_cluster_end = len(embeddings)//2
+    if n_cluster_end < (n_cluster_start + 1):
+        n_cluster_start = 2
+        n_cluster_end = n_cluster_start + 1
+    n_clusters = range(n_cluster_start, n_cluster_end)
+    for n_cluster in tqdm(n_clusters, total=len(n_clusters), desc='guess optimal clustering', disable=not verbose):
+        labels = get_model(n_cluster).fit_predict(embeddings)
+        sil_coeff = silhouette_score(embeddings, labels, metric='cosine')
+        if sil_coeff > best_sil_coeff:
+            best_sil_coeff = sil_coeff
+            best_num_clusters = n_cluster
+    if verbose:
+        print("Best N", best_num_clusters, "Best silhouette score", round(best_sil_coeff, 4))
+    return best_num_clusters
